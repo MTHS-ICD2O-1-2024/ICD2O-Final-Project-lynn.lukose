4b546bc4d2c562b137203908da26664bd7818e55
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-06-05 17:28:25 +0000

    unfinished js

--- js/gameScene.js
+++ js/gameScene.js
@@ -11,21 +11,13 @@
  */
 
 class GameScene extends Phaser.Scene {
-  // create alien
-  createAlien() {
-    const alienXLocation = Math.floor(Math.random() * 1920) + 1 // this will get a number between 1 and 1920
-    let alienXVelocity = Math.floor(Math.random() * 50) + 1 //this will get a number between 1 and 50;
-    alienXVelocity *= Math.round(Math.random()) ? 1 : -1 //this will add minus sign in 50% of cases
-    const anAlien = this.physics.add.sprite(alienXLocation, -100, "alien")
-    anAlien.body.velocity.y = 200
-    anAlien.body.velocity.x = alienXVelocity
-    this.alienGroup.add(anAlien)
-  }
   /**
    * This method is the constructor
    */
   constructor() {
     super({ key: "gameScene" })
+
+    this.background = null
   }
 
   /**
@@ -44,6 +36,8 @@ class GameScene extends Phaser.Scene {
    */
   preload() {
     console.log("Game Scene")
+
+    this.load.image('bodyBackground', './assets/bodybg.png')
   }
 
   create(data) {
