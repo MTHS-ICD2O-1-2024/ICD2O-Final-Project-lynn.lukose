6cf5c48ec406b0de878a3d606a27a981bdd1c436
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-06-05 16:30:05 +0000

    i'm trying to figure out what is wrong

--- js/menuScene.js
+++ js/menuScene.js
@@ -58,7 +58,7 @@ class MenuScene extends Phaser.Scene {
   /**
    * Should be overridden by your own Scenes.
    * This method is called once per game step while the scene is running.
-   * @param {number} time The current time.
+   * @param {number} time The current time
    * @param {number} delta The delta time in ms since the last frame.
    */
   update(time, delta) {
